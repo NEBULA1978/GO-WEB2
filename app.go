5d8308b69d2ff92cc9d2ddae97c9ca41aef9b96d
NEBULA1978 <rpascualey@gmail.com>
2023-04-24 10:12:23 +0200

    Hola mundo en localhost:3000

--- app.go
+++ app.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func main() {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(rw, "Hola Mundo")
+	})
 	http.ListenAndServe("localhost:3000", nil)
 }
+
+// Para poder retornar algo ,vamos a tomar una ruta y un handler que es una
+// función que va Lo que va a hacer es
+// devolver con un mensaje a la petición
+// del cliente
